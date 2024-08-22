0364816a76f1b181fc6144b68f2340200c5827a9
Jason D'Souza <jasondsouza2003@gmail.com>
2024-08-23 01:51:36 +0800

    0.2 - Adding API to facebook developers

--- index.js
+++ index.js
@@ -2,38 +2,49 @@
 /* ----- MODULES TO IMPORT ----- */
 const { app, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs').promises;
-//const pie = require('puppeteer-in-electron');
 const puppeteer = require('puppeteer-core');
 const path = require('node:path');
+
+/* ----- CONFIGURATIONS ----- */
+const config = {
+  'browser': 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+  'userdat': path.join(__dirname, 'userdata'),
+  'prefers': '{"browser":{"window_placement":{"bottom":925,"left":544,"maximized":false,"right":1365,"top":179,"work_area_bottom":1032,"work_area_left":0,"work_area_right":1920,"work_area_top":0}}}',
+  'inboxid': '?asset_id=382494194953080&selected_item_id=100077581671764&mailbox_id=&thread_type=FB_MESSAGE',
+}
 
 /* ----- VARIABLES ----- */
 var main, web;
 
 /* ----- MAIN BROWSER ----- */
-const loadLogin = async () => {
+const browse = async (site, end='', func=()=>{}) => {
   web.close();
-  const browser_login = await puppeteer.launch({
-      executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
-      userDataDir: path.join(__dirname, 'userdata'),
-      headless: false,
-      args: [ '--disable-infobars' ]
+  const browser = await puppeteer.launch({
+    executablePath: config['browser'],
+    userDataDir: config['userdat'],
+    headless: false,
+    args: [ '--disable-infobars', '--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu' ]
   });
-  const [page] = await browser_login.pages();
-  //await page.setViewport({ width: 1280, height: 800 });
-  await page.goto('https://business.facebook.com/login/?next=https%3A%2F%2Fbusiness.facebook.com%2F%3Fnav_ref%3Dbiz_unified_f3_login_page_to_mbs%26biz_login_source%3Dbiz_unified_f3_fb_login_button%26join_id%3Dd6177472-2033-4acc-8051-61defc4532b2');
-  await page.waitForFunction( title => document.title == title, {timeout: 0}, 'Meta Business Suite');
-  await browser_login.close();
-  main.webContents.send('login-status', 0);
-  loadBrowser();
+  const [page] = await browser.pages();
+  await page.goto(site);
+  if (end.length != 0) {
+    await page.waitForFunction( title => document.title == title, {timeout: 0}, end);
+    await browser.close();
+  }
+  func();
 }
-const loadBrowser = async () => {
-  await fs.writeFile(path.join(__dirname, 'userdata', 'Default', 'Preferences'), '{"browser":{"window_placement":{"bottom":925,"left":544,"maximized":false,"right":1365,"top":179,"work_area_bottom":1032,"work_area_left":0,"work_area_right":1920,"work_area_top":0}}}', 'utf8');
-    web = await puppeteer.launch({
-        executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
-        userDataDir: path.join(__dirname, 'userdata')
-    });
-    const page = await web.newPage();
-    await page.goto('https://business.facebook.com/latest/inbox/all');
+const stage0 = async () => {
+  await fs.mkdir(path.join(config['userdat'], 'Default'), { recursive: true });
+  await fs.writeFile(path.join(config['userdat'], 'Default', 'Preferences'), config['prefers'], 'utf8');
+  web = await puppeteer.launch({
+      executablePath: config['browser'],
+      userDataDir: config['userdat'],
+      args: [ '--disable-infobars', '--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu' ]
+  });
+  var page;
+  try {
+    page = await web.newPage();
+    await page.goto(`https://business.facebook.com/latest/inbox/all/${config['inboxid']}`);
     const title = await page.title();
     if (title == 'Facebook') {
       main.webContents.send('login-status', 1);
@@ -42,7 +53,20 @@ const loadBrowser = async () => {
       main.webContents.send('login-status', 2);
       return;
     }
-    main.webContents.send('login-status', 3);
+  } catch (e) {
+    main.webContents.send('log-status', 2);
+  }
+  main.webContents.send('login-status', 3);
+
+  // Get chats
+  await page.waitForSelector('[data-pagelet=GenericBizInboxThreadListViewBody] [role=presentation]');
+  const chats = await page.evaluate(() => [...document.querySelectorAll('[data-pagelet=GenericBizInboxThreadListViewBody] [role=presentation]')].map(x=>({
+    'name': x.querySelector(':scope>div>div:nth-child(2)>div:first-child>div:first-child').innerText,
+    'unread': window.getComputedStyle(x.querySelector(':scope>div>div:nth-child(2)>div:first-child>div:first-child>div>div>div:nth-child(2)')).fontWeight == '700',
+    'img': x.querySelector(':scope>div>div:first-child>div>div>div:first-child>img').getAttribute('src'),
+    'date': x.querySelector(':scope>div>div:nth-child(2)>div:nth-child(2)').innerText
+  })));
+  console.log(chats);
 }
 
 /* ----- MAIN WINDOW ----- */
@@ -61,11 +85,12 @@ const createWindow = () => {
   main.setTitle('DOST VI - Chatbot');
   main.setIcon('logo.png');
   main.loadFile('index.html');
-  loadBrowser();
+  stage0();
 
 
   ipcMain.on('action', (event, action) => {
-    if (action == 'login') loadLogin();
+    if (action == 'login') browse('https://business.facebook.com/login/?next=https%3A%2F%2Fbusiness.facebook.com%2F%3Fnav_ref%3Dbiz_unified_f3_login_page_to_mbs%26biz_login_source%3Dbiz_unified_f3_fb_login_button%26join_id%3Dd6177472-2033-4acc-8051-61defc4532b2', 'Meta Business Suite', stage0);
+    else if (action == 'browser') browse('https://business.facebook.com/login/?next=https%3A%2F%2Fbusiness.facebook.com%2F%3Fnav_ref%3Dbiz_unified_f3_login_page_to_mbs%26biz_login_source%3Dbiz_unified_f3_fb_login_button%26join_id%3Dd6177472-2033-4acc-8051-61defc4532b2');
   });
   //main.webContents.openDevTools();
 }
