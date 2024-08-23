97d3279163dc1b966f055b93b20c15d4a946e2c7
Jason D'Souza <jasondsouza2003@gmail.com>
2024-08-24 05:19:29 +0800

    Polished UI, Logging system, local system, etc.

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 <h1>DOST VI - Chatbot</h1>
-<style>
-details{border-left:2px solid #fff5;margin-left:10px;margin-top:30px;padding-left:10px}
-summary{position:relative;left:-15.5px;top:-20px;margin-bottom:-25px;}
-</style>
-<details open>
+<details>
+  <summary><b>Introduction</b></summary>
+  <h2>Organizers</h2>
+  <table>
+    <tr>
+      <td><img src="asset/dost.png" width="100"></img></td>
+      <td><img src="asset/nstw.png" width="100"></img></td>
+      <td><img src="asset/usa.png" width="100"></img></td>
+      <td><img src="asset/bscpe.png" width="100"></img></td>
+    </tr>
+    <tr style="text-align:center">
+      <td>DOST</td>
+      <td>NSTW</td>
+      <td>USA</td>
+      <td>BSCPE</td>
+    </tr>
+  </table>
+</details>
+<details>
   <summary><b>Problems and Solutions</b></summary>
-  <details open>
+  <details>
     <summary><b>Current solutions to problems</b></summary>
     <table>
         <tr><th>Problem</th><th>Solution</th></tr>
@@ -14,7 +28,7 @@ summary{position:relative;left:-15.5px;top:-20px;margin-bottom:-25px;}
         <tr><td>Not everyone speaks or understands English, particularly those who need help the most</td><td>By utilizing a multilingual language model, the chatbot can interact in various languages, making DOST processes, programs, scholarships, and services accessible to non-English speakers.</td></tr>
     </table>
   </details>
-  <details open>
+  <details>
     <summary><b>Future solutions to problems</b></summary>
     <table>
         <tr><th>Problem</th><th>Solution</th></tr>
@@ -26,7 +40,21 @@ summary{position:relative;left:-15.5px;top:-20px;margin-bottom:-25px;}
 </details>
 <details open>
   <summary><b>Technical Explaination</b></summary>
-  <details open>
+  <details>
+    <summary><b>Stages of development</b></summary>
+    <ol>
+      <li>We first downloaded our dataset and convert them from PDF to images, markdowns, texts, etc. mostly locally though softwares like <code>pandoc</code> or <code>convert</code> so that we can later use them to train the model</li>
+      <li>We downloaded <code>Ollama</code> and tested serveral pre-existing LLM/NLP to act has our base LLM to train our fine-tuined model like <code>Llama</code>, <code>Mistral</code>, and <code>Llava</code></li>
+      <li>We trail and error different <code>modelfile</code>s on different LLM models and decided to stick to <code>LLama3 7b</code> for it was the most advance LLM/NLP we can host without it being too slow or dumb. This step of the process happened in parallel with the UI development because it took a long time to fine-tune it to our desired model</code>
+      <li>The best platform to make this project was <code>Python</code> for it support in AI frameworks, <code>Javascript</code> for its adaptibility in most systems, and <code>C/C++</code> for its optimizations and efficency. Python is slow, limited interface<i>(tkinter)</i>, and lacked packability in different systems. C/C++ is good but would take alot of time thus not suited for our limited time range. Thus we picked Javascript, more specifically Node JS for its diversity in packages<i>(NPM)</i></li>
+      <li>We created an <code>Electron</code> project to allow future packability in different systems especially servers</li>
+      <li>We then created the interface in pure <code>HTML</code>, <code>JS</code>, and <code>CSS</code> for the electron to display</li>
+      <li>We connected the interface to <code>Ollama</code> with our own custom model named <code>dost</code> to a chating interface</li>
+      <li>We connected <code>Puppeteer</code> to control a facebook Page named <code>DOST-VI not legit</code> through the interface</li>
+      <li>We finally compiled it to our desired platform in this case windows</li>
+    </ol>
+  </details>
+  <details>
     <summary><b>Technologies used</b></summary>
     <table>
       <tr><th>Technology</th><th>Purpose</th><th>Reason</th></tr>
@@ -36,7 +64,7 @@ summary{position:relative;left:-15.5px;top:-20px;margin-bottom:-25px;}
       <tr><td>Puppeteer</td><td>Automation and emulating APIs</td><td>APIs of services like Facebook, Instagram, etc. are not free so headless automating browsers allows us to use them for free</td></tr>
     </table>
   </details>
-  <details open>
+  <details>
     <summary><b>Challenges we faced</b></summary>
     <table>
       <tr><th>Challenge</th><th>Explaination/Solution</th></tr>
@@ -44,19 +72,6 @@ summary{position:relative;left:-15.5px;top:-20px;margin-bottom:-25px;}
       <tr><td>Training our own custom model</td><td>To train or fine-tune the model on the dataset of four provided documents, we applied some LLM techniques like Chunking, Few Shot Learning, and Prompt engineering to enhance the efficiency and accuracy of the information that's retrieved and processed in the NLP. The hardest data set to train on was the RSTL services document because is was the largest among the four</td></tr>
       <tr><td>Graphical Designing</td><td>Since our groups consist primarly of Computer engineering students we lacked the human resources to design a good graphical user interface so we had to spend alot of time planning and trail and error a good looking interface. Resulting into a minimalistic simple interface</td></tr>
     </table>
-  </details>
-  <details open>
-    <summary><b>Processsing stages</b></summary>
-    <ol>
-      <li>We first downloaded our dataset and convert them from PDF to images, markdowns, texts, etc. mostly locally though softwares like <code>pandoc</code> or <code>convert</code> so that we can later use them to train the model</li>
-      <li>We downloaded <code>Ollama</code> and tested serveral pre-existing LLM/NLP to act has our base LLM to train our fine-tuined model like <code>Llama</code>, <code>Mistral</code>, and <code>Llava</code></li>
-      <li>We trail and error different <code>modelfile</code>s on different LLM models and decided to stick to <code>LLama3 7b</code> for it was the most advance LLM/NLP we can host without it being too slow or dumb. This step of the process happened in parallel with the UI development because it took a long time to fine-tune it to our desired model</code>
-      <li>The best platform to make this project was <code>Python</code> for it support in AI frameworks, <code>Javascript</code> for its adaptibility in most systems, and <code>C/C++</code> for its optimizations and efficency. Python is slow, limited interface<i>(tkinter)</i>, and lacked packability in different systems. C/C++ is good but would take alot of time thus not suited for our limited time range. Thus we picked Javascript, more specifically Node JS for its diversity in packages<i>(NPM)</i></li>
-      <li>We created an <code>Electron</code> project to allow future packability in different systems especially servers</li>
-      <li>We then created the interface in pure <code>HTML</code>, <code>JS</code>, and <code>CSS</code> for the electron to display</li>
-      <li>We connected the interface to <code>Ollama</code> with our own custom model named <code>dost</code> to a chating interface</li>
-      <li>We connected <code>Puppeteer</code> to a facebook Page named <code>DOST-VI not legit</code></li>
-    </ol>
   </details>
   <details open>
     <summary><b>User interface</b></summary>
@@ -68,3 +83,12 @@ summary{position:relative;left:-15.5px;top:-20px;margin-bottom:-25px;}
     <summary><b>Current Limitations</b></summary>
   </details>
 </details>
+
+
+
+
+
+<style>
+details{border-left:2px solid #fff5;margin-left:10px;margin-top:30px;padding-left:10px}
+summary{position:relative;left:-15.5px;top:-20px;margin-bottom:-25px;}
+</style>
--- src/index.js
+++ src/index.js
@@ -5,50 +5,68 @@ const fs = require('fs').promises;
 const puppeteer = require('puppeteer-core');
 const path = require('node:path');
 const {default: ollama} = require('ollama');
+const showdown = require('showdown');
 
 /* ----- CONFIGURATIONS ----- */
 const config = {
   'browser': 'C:/Program Files/Google/Chrome/Application/chrome.exe',
-  'userdat': path.join(__dirname, 'userdata'),
+  'userdat': path.join(process.env.APPDATA, 'dost-vi-chatbot'), //path.join(__dirname, 'userdata'),
   'prefers': '{"browser":{"window_placement":{"bottom":925,"left":544,"maximized":false,"right":1365,"top":179,"work_area_bottom":1032,"work_area_left":0,"work_area_right":1920,"work_area_top":0}}}',
   'inboxid': '?asset_id=382494194953080&selected_item_id=100077581671764&mailbox_id=&thread_type=FB_MESSAGE',
   'sleepms': 2000,
   'timeout': 0,
-}
+  'fmodels': ['tinyllama'],
+  'aigreet': "😊 Hi there! I'm Chatbot by DOST, your helpful assistant for all things related to the Department of Science and Technology (DOST). I'd be delighted to assist you with any questions or concerns about DOST programs, scholarships, and services. What's on your mind? 🤔",
+  'devtool': true,
+};
 
 /* ----- VARIABLES ----- */
-var main, web, chats = {
-  '': [
-    {'role':'assistant', 'content':"😊 Hi there! I'm Chatbot by DOST, your helpful assistant for all things related to the Department of Science and Technology (DOST). I'd be delighted to assist you with any questions or concerns about DOST programs, scholarships, and services. What's on your mind? 🤔"}
-  ]
+var main, web, browser, chats = {
+  '': [ {'role':'assistant', 'content':config['aigreet']} ] // Conversation by no user but the interface
 };
+var md2html = new showdown.Converter();
 
 /* ----- MAIN BROWSER ----- */
 const browse = async (site, end='', func=()=>{}) => {
-  main.webContents.send('login-status', 0);
-  web.close();
-  const browser = await puppeteer.launch({
+  if (web != undefined && web.close != undefined) web.close();
+  browser = await puppeteer.launch({
     executablePath: config['browser'],
     userDataDir: config['userdat'],
-    headless: false,
+    headless: typeof end == 'number',
     args: [ '--disable-infobars', '--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu' ]
+  });
+  browser.on('disconnected', ()=> {
+    console.log('Browser closed');
+    main.webContents.send('action', 'browser closed');
+    stage0();
   });
   const [page] = await browser.pages();
   await page.goto(site);
-  if (end.length != 0) {
+  if (typeof end == 'string' && end.length != 0) {
     await page.waitForFunction( title => document.title == title, {timeout: config['timeout']}, end);
     await browser.close();
-  }
-  func();
+    func(null, null);
+  } else func(page, browser);
 }
 const stage0 = async () => {
+  // Set chrome userdata
   await fs.mkdir(path.join(config['userdat'], 'Default'), { recursive: true });
   await fs.writeFile(path.join(config['userdat'], 'Default', 'Preferences'), config['prefers'], 'utf8');
+  
+  // Set config variable "browser"
+  try {
+    const f = await fs.readFile(path.join(config['userdat'], 'chrome_path.txt'), 'utf8');
+    config['browser'] = f;
+  } catch (err) {
+    if (err.code == 'ENOENT') await fs.writeFile(path.join(config['userdat'], 'chrome_path.txt'), config['browser'], 'utf8');
+  }
+  main.webContents.send('config-browser', config['browser']);
+
   web = await puppeteer.launch({
       executablePath: config['browser'],
       userDataDir: config['userdat'],
       args: [ '--disable-infobars', '--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu' ],
-      headless: false,
+      //headless: false,
   });
   var page;
   try {
@@ -62,10 +80,10 @@ const stage0 = async () => {
       main.webContents.send('login-status', 2);
       return;
     }
+    main.webContents.send('login-status', 3);
   } catch (e) {
-    main.webContents.send('log-status', 2);
+    main.webContents.send('login-status', 2);
   }
-  main.webContents.send('login-status', 3);
 
   // Wait for chats to load
   await page.waitForSelector('[data-pagelet=GenericBizInboxThreadListViewBody] [role=presentation]', {timeout:config['timeout']});
@@ -107,7 +125,7 @@ const stage0 = async () => {
     if (chats[chat['name']] == undefined) chats[chat['name']] = [{role:'user',content: msg}];
     else chats[chat['name']].push({role:'user',content: msg});
     const res = await ollama.chat({
-      model: 'dost',
+      model: config['fmodels'][0],
       messages: chats[chat['name']],
     });
     chats[chat['name']].push(res.message);
@@ -118,6 +136,10 @@ const stage0 = async () => {
       res.message.content += '--- Chat terminated, cleared memory ---';
       delete chats[chat['name']];
     }
+
+    // Record log
+    var date = new Date();
+    main.webContents.send('log', [`${date.getFullYear()}/${String(date.getMonth()).padStart(2,'0')}/${String(date.getDate())} ${String(date.getHours()).padStart(2,'0')}:${String(date.getMinutes()).padStart(2,'0')}:${String(date.getSeconds()).padStart(2,'0')}`, chat['name'], res.message.content]);
 
     // Send message
     await page.waitForSelector('[data-pagelet="BizP13NInboxMessengerDetailView"] textarea', {timeout:config['timeout']});
@@ -140,8 +162,8 @@ const stage0 = async () => {
   }
 }
 
-/* ----- MAIN WINDOW ----- */
-const createWindow = () => {
+/* ----- Creates the Graphical Window ----- */
+const create_window = () => {
   main = new BrowserWindow({
     width: 800,
     height: 600,
@@ -154,33 +176,63 @@ const createWindow = () => {
   });
   main.setMenu(null);
   main.setTitle('DOST VI - Chatbot');
-  main.setIcon('logo.png');
+  main.setIcon('asset/dost.png');
   main.loadFile('index.html');
-  //stage0();
+  stage0();
 
+  main.webContents.send('chatbot', config['aigreet']);
 
-  ipcMain.on('action', (event, action) => {
+  ipcMain.on('action', async (event, action) => {
     if (action == 'login') browse('https://business.facebook.com/login/?next=https%3A%2F%2Fbusiness.facebook.com%2F%3Fnav_ref%3Dbiz_unified_f3_login_page_to_mbs%26biz_login_source%3Dbiz_unified_f3_fb_login_button%26join_id%3Dd6177472-2033-4acc-8051-61defc4532b2', 'Meta Business Suite', stage0);
-    else if (action == 'browser') browse('https://business.facebook.com/login/?next=https%3A%2F%2Fbusiness.facebook.com%2F%3Fnav_ref%3Dbiz_unified_f3_login_page_to_mbs%26biz_login_source%3Dbiz_unified_f3_fb_login_button%26join_id%3Dd6177472-2033-4acc-8051-61defc4532b2');
+    else if (action == 'browser') {
+      main.webContents.send('login-status', 0);
+      browse('https://business.facebook.com/login/?next=https%3A%2F%2Fbusiness.facebook.com%2F%3Fnav_ref%3Dbiz_unified_f3_login_page_to_mbs%26biz_login_source%3Dbiz_unified_f3_fb_login_button%26join_id%3Dd6177472-2033-4acc-8051-61defc4532b2');
+    } else if (action == 'browser closed' && browser != undefined && browser.close != undefined) browser.close();
+    else if (action == 'resetchat') {
+      chats[''] = [{'role':'assistant', 'content':config['aigreet']}];
+      main.webContents.send('chatbot', config['aigreet']);
+    } else if (action == 'logout') {
+      main.webContents.send('login-status', 0);
+      browse('https://business.facebook.com/', 0, async (page,browser) => {
+        // Check if logged in
+        var title = await page.title();
+        if (title == 'Facebook') return await browser.close();
+        // Click account options
+        await page.waitForSelector('[data-pagelet="BizKitPresenceSelector"] a[href]', {timeout:config['timeout']});
+        await page.click('[data-pagelet="BizKitPresenceSelector"] a[href]');
+        // Click logout button
+        await page.waitForSelector('.uiContextualLayerPositioner  [data-surface="/bizweb:home"] div:last-child>[role="button"]', {timeout:config['timeout']});
+        await page.click('.uiContextualLayerPositioner  [data-surface="/bizweb:home"] div:last-child>[role="button"]');
+        // Success
+        var title = await page.title();
+        if (title != 'Facebook') main.webContents.send('log-status', 2);
+        else await browser.close();
+      });
+    }
   });
   ipcMain.on('chatbot', async (event, msg) => {
     chats[''].push({role:'user',content: msg});
     console.log('Making request to ollama');
     const res = await ollama.chat({
-      model: 'dost',
+      model: config['fmodels'][0],
       messages: chats[''],
     });
     chats[''].push(res.message);
-    main.webContents.send('chatbot', res.message.content);
+    main.webContents.send('chatbot', md2html.makeHtml(res.message.content));
+  });
+  ipcMain.on('config-browser', async (event, data) => {
+    await fs.writeFile(path.join(config['userdat'], 'chrome_path.txt'), data, 'utf8')
+    config['browser'] = data;
   })
-  main.webContents.openDevTools();
+  if (config['devtool']) main.webContents.openDevTools();
 }
 
 
+/* ----- WINDOW EVENTS ----- */
 app.whenReady().then(() => {
-  createWindow()
+  create_window()
   app.on('activate', () => { // For MacOS
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) create_window()
   })
 })
 app.on('window-all-closed', async () => { // For MacOS
