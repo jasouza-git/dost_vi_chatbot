fb4a95df84188ad54574af5018bc894778d1cf4b
GOJOSTRONG <oplokatyt@gmail.com>
2024-08-23 14:46:19 +0800

    Uploading mascot and her hand

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const stage0 = async () => {
       executablePath: config['browser'],
       userDataDir: config['userdat'],
       args: [ '--disable-infobars', '--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu' ],
-      //headless: false,
+      headless: false,
   });
   var page;
   try {
